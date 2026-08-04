09ea8b2ead40956af4c971003c95a8a7f38284a5
Mariano <marianopainefil@gmail.com>
2026-08-03 22:15:50 -0300

    feat: foto de Lorena en sección Sobre mí

--- src/app/home/essence-section/essence-section.ts
+++ src/app/home/essence-section/essence-section.ts
@@ -8,5 +8,5 @@ import { RevealDirective } from '../../shared/reveal.directive';
   styleUrl: './essence-section.scss',
 })
 export class EssenceSection {
-  protected readonly imageSrc = '/lorena-thumb.svg';
+  protected readonly imageSrc = '/lorena_aguera.jpeg';
 }
